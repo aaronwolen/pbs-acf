bb2dc7bd92dfd65c0f30f50012f3d2f2c9bc07dd
Aaron Wolen <aaron@wolen.com>
2019-11-04 14:38:02 -0600

    Refactor pbs-status
    Directly parses qstat raw output  and converts to a dict

--- {{cookiecutter.profile_name}}/pbs-status.py
+++ {{cookiecutter.profile_name}}/pbs-status.py
@@ -2,17 +2,33 @@
 
 import sys
 import subprocess
-import xml.etree.cElementTree as ET
 
 jobid = sys.argv[1]
 
+def make_qstat_dict (res):
+    try:
+        status = res.stdout.decode()
+    except:
+        print("res must be a 'subprocess.CompletedProcess'")
+
+    # Every line but the first uses '=' to delimit key/value pairs
+    status = status.replace('Id:', 'Id =')
+    out = {k.strip():v.strip() for k,v in (row.split(' = ') for row in status.split('\n    '))}
+    return out
+
 try:
-    res = subprocess.run("mdiag --format=XML -j {}".format(jobid), check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    res = subprocess.run("qstat -f {}".format(jobid), check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    job_status = make_qstat_dict(res)
+    job_state = job_status.get('job_state')
 
-    xmldoc = ET.ElementTree(ET.fromstring(res.stdout.decode())).getroot()
-    job_state = xmldoc.findall('.//State')[0].text
-
-    print(job_state)
+    if job_state == "C":
+        exit_status = job_status.get('exit_status')
+        if exit_status == '0':
+            print("success")
+        else:
+            print("failed")
+    else:
+        print("running")
 
 except (subprocess.CalledProcessError, IndexError, KeyboardInterrupt) as e:
     print("failed")
